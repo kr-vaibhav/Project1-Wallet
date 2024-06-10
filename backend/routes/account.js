520e6a6fd5cfea56a16e4b84b7147d8586845ccc
kr-vaibhav <kumarvaibhav.2102@gmail.com>
2024-06-10 19:25:16 +0530

    account code updated

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -18,9 +18,10 @@ router.get("/balance", authMiddleware, async (req, res) => {
 router.post("/transfer", authMiddleware, async (req, res) => {
     const session = await mongoose.startSession();
 
-   try {session.startTransaction();
+    session.startTransaction();
     const { amount, to } = req.body;
 
+    // Fetch the accounts within the transaction
     const account = await Account.findOne({ userId: req.userId }).session(session);
 
     if (!account || account.balance < amount) {
@@ -32,14 +33,6 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
     const toAccount = await Account.findOne({ userId: to }).session(session);
 
-    if( account.userId.toString()  === toAccount.userId.toString() ){
-        await session.abortTransaction();
-        session.endSession();
-        return res.status(400).json({
-            message: "Transferring to same account"
-        })
-    }
-
     if (!toAccount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -47,27 +40,15 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         });
     }
 
-    account.balance -= amount;
-    toAccount.balance += amount;
+    // Perform the transfer
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
 
-    await account.save({ session });
-    await toAccount.save({ session });
-
-    // await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    // await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
+    // Commit the transaction
     await session.commitTransaction();
-    session.endSession();
     res.json({
         message: "Transfer successful"
     });
-    }catch(err){
-        await session.abortTransaction();
-        session.endSession();
-        res.status(500).json({
-            message: "Internal server error"
-        });
-    }
 });
 
 module.exports = router;
