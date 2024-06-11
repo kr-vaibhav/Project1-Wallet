c6efccac0d173a10b1b674e10e859580f0de9846
kr-vaibhav <kumarvaibhav.2102@gmail.com>
2024-06-11 12:14:47 +0530

    changes made again :-)

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -4,7 +4,7 @@ export function InputBox( { label, placeholder, onchange } ){
         <div className="text-sm font-medium text-left py-2">
             {label}
         </div>
-        <input onchange={onchange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+        <input onChange={onchange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
 
     </div>
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -13,19 +13,20 @@ export const Signup = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    
 
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign up"} />
         <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox onChange={e => {
+        <InputBox onChange={(e) => {
           setFirstName(e.target.value);
-        }} placeholder="John" label={"First Name"} />
+        }} placeholder="Kumar" label={"First Name"} />
         <InputBox onChange={(e) => {
           setLastName(e.target.value);
-        }} placeholder="Doe" label={"Last Name"} />
-        <InputBox onChange={e => {
+        }} placeholder="Vaibhav" label={"Last Name"} />
+        <InputBox onChange={(e) => {
           setUsername(e.target.value);
         }} placeholder="Vaibhav@gmail.com" label={"Email"} />
         <InputBox onChange={(e) => {
